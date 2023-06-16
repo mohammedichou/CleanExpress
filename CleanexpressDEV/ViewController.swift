3f14d7e9c04e85f0fa30149c2a7151ae0a1ece4e
mohammedichou <devichoumohammed@gmail.com>
2023-06-17 01:27:54 +0200

    Initial Commit

---
+++ CleanexpressDEV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanexpressDEV
+//
+//  Created by mohammed ichou on 17/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
